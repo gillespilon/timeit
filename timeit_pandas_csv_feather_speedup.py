7a10b1d0e6c0a82f7c82a6ecb660c795f1587270
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-17 13:06:54 -0600

    Add functions annotations, tidy cody

--- timeit_pandas_csv_feather_speedup.py
+++ timeit_pandas_csv_feather_speedup.py
@@ -5,6 +5,7 @@ feather file and by downcasting integer columns and convert an object column
 to a category column.
 """
 
+from typing import NoReturn
 from pathlib import Path
 import timeit
 import time
@@ -29,7 +30,10 @@ def f2(
     column: str,
     value: str
 ) -> pd.DataFrame:
-    df = df.set_index(keys=column, drop=False)
+    df = df.set_index(
+        keys=column,
+        drop=False
+    )
     df = df.loc[value]
     return df
 
@@ -46,7 +50,7 @@ def f3(
 def compare_functions(
     repetitions: int,
     replications: int
-):
+) -> NoReturn:
     start_time = time.perf_counter()
     stmt_f1 = 'result = f1(df=df, column="category", value="A")'
     stmt_f1 = 'result = f2(df=df, column="category", value="A")'
@@ -86,16 +90,31 @@ def compare_functions(
     print()
 
 
-def create_large_csv_file(path_csv: Path):
-    df = pd.DataFrame(np.random.randint(0, 100, size=(10000000, 50)))
+def create_large_csv_file(
+    path_csv: Path,
+    rows_per_category: int
+) -> NoReturn:
+    df = pd.DataFrame(
+        data=np.random.randint(
+            low=0,
+            high=100,
+            size=(10000000, 50)
+        )
+    )
     df = df.rename(columns={i: f"x_{i}" for i in range(50)})
-    df["category"] = ["A", "B", "C", "D"] * 2500000
-    ds.save_file(df=df, file_name=path_csv)
+    df["category"] = ["A", "B", "C", "D"] * rows_per_category
+    ds.save_file(
+        df=df,
+        file_name=path_csv
+    )
     print("Create a DataFrame and save as a csv file")
     print()
 
 
-def read_large_csv_file(path_csv: Path):
+def read_large_csv_file(
+    path_csv: Path,
+    chunksize: int
+) -> pd.DataFrame:
     print("Reading csv file chunks")
     start_time = time.perf_counter()
     chunk = pd.read_csv(
@@ -132,7 +151,7 @@ def read_large_csv_file(path_csv: Path):
     return df
 
 
-def optimize_columns(df: pd.DataFrame):
+def optimize_columns(df: pd.DataFrame) -> pd.DataFrame:
     df = ds.optimize_columns(df=df)
     print("Information about the DataFrame after optimization of columns")
     print(df.info(verbose=False))
@@ -142,8 +161,11 @@ def optimize_columns(df: pd.DataFrame):
 def save_csv_file_as_feather_file(
     df: pd.DataFrame,
     path_feather: Path
-):
-    ds.save_file(df=df, file_name=path_feather)
+) -> pd.DataFrame:
+    ds.save_file(
+        df=df,
+        file_name=path_feather
+    )
     print("Save the DataFrame as a feather file")
     print()
     return df
@@ -151,7 +173,7 @@ def save_csv_file_as_feather_file(
 
 def read_large_feather_file(
     path_feather: Path
-):
+) -> pd.DataFrame:
     print("Read feather file, create DataFrame")
     start_time = time.perf_counter()
     df = ds.read_file(file_name=path_feather)
@@ -179,10 +201,15 @@ def main():
     global df
     path_feather = Path("very_large.feather")
     path_csv = Path("very_large.csv")
+    rows_per_category = 2500000
+    chunksize = 1000000
     repetitions = 10
     replications = 5
     gc.enable()
-    # create_large_csv_file(path_csv=path_csv)
+    # create_large_csv_file(
+    #     path_csv=path_csv,
+    #     rows_per_category-rows_per_category
+    # )
     # df = read_large_csv_file(path_csv=path_csv)
     # df = optimize_columns(df=df)
     # save_csv_file_as_feather_file(
@@ -193,12 +220,21 @@ def main():
     print("---------------------------")
     print()
     df = read_large_feather_file(path_feather=path_feather)
-    compare_functions(repetitions=repetitions, replications=replications)
+    compare_functions(
+        repetitions=repetitions,
+        replications=replications
+    )
     print("Analysis using csv file")
     print("-----------------------")
     print()
-    df = read_large_csv_file(path_csv=path_csv)
-    compare_functions(repetitions=repetitions, replications=replications)
+    df = read_large_csv_file(
+        path_csv=path_csv,
+        chunksize=chunksize
+    )
+    compare_functions(
+        repetitions=repetitions,
+        replications=replications
+    )
 
 
 if __name__ == "__main__":
